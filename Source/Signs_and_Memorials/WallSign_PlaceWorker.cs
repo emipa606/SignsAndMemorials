3c51618292c698add985658c80f16d5a21ba7efd
Mlie <emipa606@users.noreply.github.com>
2023-12-20 21:52:28 +0100

    Added wallmounted signs. Used DeepL to update translations for ChineseSimplified, French, German

---
+++ Source/Signs_and_Memorials/WallSign_PlaceWorker.cs
@@ -0,0 +1,48 @@
+﻿using Verse;
+
+namespace SaM;
+
+public class WallSign_PlaceWorker : PlaceWorker
+{
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map,
+        Thing thingToIgnore = null, Thing thing = null)
+
+    {
+        var c = loc;
+
+        var support = c.GetEdifice(map);
+        if (support == null)
+        {
+            return "SaM_Placeworker_OnSupport".Translate();
+        }
+
+        if (support.def?.graphicData == null)
+        {
+            return "SaM_Placeworker_OnSupport".Translate();
+        }
+
+        if ((support.def.graphicData.linkFlags & (LinkFlags.Rock | LinkFlags.Wall)) == 0)
+        {
+            return "SaM_Placeworker_OnSupport".Translate();
+        }
+
+        c = loc + rot.Opposite.FacingCell;
+        if (!c.Walkable(map))
+        {
+            return "SaM_Placeworker_Walkable".Translate();
+        }
+
+        var currentBuildings = loc.GetThingList(map);
+        foreach (var building in currentBuildings)
+        {
+            if (building?.def?.defName != "SaM_WallSign")
+            {
+                continue;
+            }
+
+            return "SaM_Placeworker_Existing".Translate();
+        }
+
+        return AcceptanceReport.WasAccepted;
+    }
+}
